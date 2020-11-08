0fbd1175dbbeec8c7aa83262b9cefccd3996748b
Jasse Wistrand <jasse.wistrand@iki.fi>
2020-11-08 14:44:52 +0100

    Struct opencard to manage card files

--- src/model/opencard.rs
+++ src/model/opencard.rs
@@ -2,6 +2,62 @@ use std::path::{PathBuf, Path};
 use std::fs;
 use std::io;
 use super::cardface::CardFace;
+use rusqlite::{Connection, params};
+use super::cardface;
+
+pub struct CardFolder {
+    pub folder: PathBuf
+}
+
+impl CardFolder {
+    pub fn new(folder: PathBuf) -> CardFolder {
+        if folder.is_dir() {
+            return CardFolder { folder };
+        } else {
+            // Note(wistrandj): This is not the way to handle errors.
+            panic!("The default card folder is missing");
+        }
+    }
+
+    pub fn from_timeline(timeline: &Connection) -> CardFolder {
+        let row = timeline.query_row(
+            "select default_location from configuration",
+            params![],
+            |row| {
+                let folder: String = row.get(0)?;
+                let folder = PathBuf::from(folder);
+                return Ok(folder);
+            });
+
+        if let Ok(path) = row {
+            let folder = PathBuf::from(path);
+            return Self::new(folder);
+        } else {
+            panic!("Bug: The default card folder is not configured");
+        }
+    }
+
+    pub fn cards(&self) -> Vec<cardface::CardFace> {
+        let cardnames = list_file_names(&self.folder);
+
+        if let Ok(cardnames) = cardnames {
+            let cards: Vec<CardFace> = cardnames.iter()
+                .map(|it| cardface::CardFace::from_name(it))
+                .filter(|it| it.is_some())
+                .map(|it| it.unwrap())
+                .collect();
+            return cards;
+        } else {
+            panic!("No cards");
+        }
+    }
+}
+
+pub fn next_available(cards: &Vec<cardface::CardFace>) -> CardFace {
+    let latest_card = cards.iter().max();
+    let latest_number: usize = if let Some(card) = latest_card { card.major_number() } else { 0 };
+    return CardFace::from_number(latest_number + 1);
+}
 
 pub fn next_major_card(folder: &PathBuf) -> CardFace {
     let cards = list_cards(folder);
--- src/zkcard.rs
+++ src/zkcard.rs
@@ -2,46 +2,22 @@
 //   $ zk-card --timeline ./timeline.zk card
 
 use std::path::PathBuf;
-use rusqlite::{Connection, params};
+use rusqlite::Connection;
 
 use super::file;
 use super::config;
 use super::model::opencard;
-use super::model::cardface::CardFace;
 use super::model::dbcard;
-
-
-#[derive(Debug)]
-struct A {
-    default_location: String
-}
-
-fn opencard_folder(timeline: &Connection) -> Option<PathBuf> {
-    let mut query_location = timeline.prepare("select default_location from configuration;").unwrap();
-    let location = query_location.query_row(params![], |row| {
-        Ok(A {
-            default_location: row.get(0)?
-        })
-    });
-    let location = PathBuf::from(location.unwrap().default_location);
-    if !location.is_dir() {
-        eprintln!("The location for open cards {} is missing", location.to_str().unwrap());
-        return None;
-    }
-
-    return Some(location);
-}
 
 pub fn zkcard(timeline_file: PathBuf) {
     let mut timeline: Connection = config::open_timeline(&timeline_file).unwrap();
-    let location = opencard_folder(&timeline).unwrap();
-    let next_card: CardFace = opencard::next_major_card(&location);
-    let next_card_location: PathBuf = next_card.location_in(&location);
-
-    println!("Open a new card in {}", next_card.name());
-
-    file::make_template(&next_card_location);
-    file::edit(&next_card_location);
-    dbcard::save_cards(&mut timeline, &vec![next_card]);
+    let opencards = opencard::CardFolder::from_timeline(&timeline);
+    let cards = opencards.cards();
+    let next = opencard::next_available(&cards);
+    let next_location: PathBuf = next.location_in(&opencards.folder);
+    eprintln!("Open a new card in {}", next.name());
+    file::make_template(&next_location);
+    file::edit(&next_location);
+    dbcard::save_cards(&mut timeline, &vec![next]);
 }
 
