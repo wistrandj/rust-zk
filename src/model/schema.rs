565bc6543491805f082557837c15ad4150b72e92
Jasse Wistrand <jasse.wistrand@iki.fi>
2020-12-06 19:09:15 +0100

    Add tag subcommand
    The tag subcommand can tag given cards and it has few switches:
    
        --list    Print tags by given tags
        --show    Print cards by given tags
        --delete  Delete a tag from given card(s) or from all of them

--- src/model/schema.rs
+++ src/model/schema.rs
@@ -1,5 +1,7 @@
 use rusqlite::{Connection, params};
 use crate::feature as feature;
+use std::collections::HashSet;
+
 
 pub fn install_missing_features(conn: &mut Connection) {
     if !feature::has_feature_table("feature", &conn) {
@@ -7,6 +9,7 @@ pub fn install_missing_features(conn: &mut Connection) {
     }
     feature::enable_feature("setup1", conn, &Setup1 {});
     feature::enable_feature("setup2", conn, &Setup2 {});
+    feature::enable_feature("tag", conn, &Tag {});
 }
 
 struct Setup1 {}
@@ -70,6 +73,247 @@ impl feature::Feature for Setup2 {
 
         if let Err(msg) = success {
             panic!("Fail to delete card and content tables. Reason: {}", msg);
+        }
+    }
+}
+
+pub struct TagFeature<'a> {
+    connection: &'a Connection
+}
+
+impl<'a> TagFeature<'a> {
+    pub fn new(connection: &'a Connection) -> TagFeature<'a> {
+        TagFeature { connection }
+    }
+
+    pub fn all_tags(&self) -> Result<Vec<String>, &'static str> {
+        let mut tags_set: HashSet<String> = HashSet::new();
+        let mut tags_vec: Vec<String> = Vec::new();
+
+        let sql = "select tag_name from tag order by 1;";
+        let args = params![];
+        let mut stmt = self.connection.prepare(sql);
+        if let Ok(mut stmt) = stmt {
+            let tags_result = stmt.query_map(args,
+                |row| {
+                    let tag_name: String = row.get(0)?;
+                    return Ok(tag_name);
+                }
+            ).unwrap();
+
+            for tag in tags_result {
+                tags_set.insert(tag.unwrap());
+            }
+            for tag in tags_set.drain() {
+                tags_vec.push(tag);
+            }
+            tags_vec.sort();
+            return Ok(tags_vec);
+        } else {
+            return Err("Fail to prepare a query");
+        }
+    }
+
+    /// Given a list of cards, find all tags associated to them.
+    pub fn find_tags_of_cards(&self, major_card_numbers: &Vec<usize>) -> Result<Vec<String>, &'static str> {
+        let mut found_tags = HashSet::new();
+
+        let sql = "select tag_name from tag where major_card_number = ?1";
+        for &major_card in major_card_numbers {
+            let major_card: u32 = major_card as u32;
+            let args = params![major_card];
+            let mut stmt = self.connection.prepare(sql).unwrap();
+            let result = stmt.query_map(args, |row| {
+                let tag_name: String = row.get(0)?;
+                return Ok(tag_name);
+            });
+
+            for tag_name_row in result.unwrap() {
+                let tag_name: String = tag_name_row.unwrap();
+                found_tags.insert(tag_name);
+            }
+        }
+
+        let mut found_tags_vec = Vec::new();
+        for tag_name in found_tags.drain() {
+            found_tags_vec.push(tag_name);
+        }
+        found_tags_vec.sort();
+        return Ok(found_tags_vec);
+    }
+
+    pub fn find_all_cards_for_given_tags(&self, tag_names: &Vec<String>) -> Result<Vec<usize>, &'static str> {
+        let mut major_cards_set: HashSet<usize> = HashSet::new();
+
+        let sql = "select major_card_number from tag where tag_name = ?1";
+        let mut stmt = self.connection.prepare(sql).unwrap();
+
+        for tag_name in tag_names {
+            let args = params![tag_name];
+            let rows = stmt.query_map(args, |row| {
+                let major_card_number: u32 = row.get(0)?;
+                return Ok(major_card_number as usize);
+            }).unwrap();
+
+            for row in rows {
+                let major_card: usize = row.unwrap();
+                major_cards_set.insert(major_card);
+            }
+        }
+
+        let mut major_cards_vec = Vec::new();
+        for major_card in major_cards_set.drain() {
+            major_cards_vec.push(major_card);
+        }
+        major_cards_vec.sort();
+        return Ok(major_cards_vec);
+    }
+
+    pub fn tag_exists(&self, tag_name: &str) -> Result<bool, &'static str> {
+        let sql = "select count(*) from tag where tag_name = ?1";
+        let args = params![tag_name];
+        let success = self.connection.query_row(sql, args, |row| {
+            let count: u32 = row.get(0)?;
+            return Ok(count);
+        });
+        match success {
+            Ok(count) => Ok(count > 0),
+            Err(_) => Err("Fail to check if the given tag exists in a database")
+        }
+    }
+
+    pub fn tag_is_set(&self, tag_name: &str, major_card_number: usize) -> Result<bool, &'static str> {
+        let sql = "select count(*) from tag where tag_name = ?1 and major_card_number = ?2";
+        let args = params![tag_name, major_card_number as u32];
+        let tag_exists = self.connection.query_row(sql, args,
+            |row| {
+                let matching_rows: u32 = row.get(0)?;
+                println!("{}", format!("> tags for {} are count of {}", major_card_number, matching_rows));
+                return Ok(matching_rows > 0);
+            });
+        match tag_exists {
+            Ok(tag_exists) => Ok(tag_exists),
+            Err(_) => Err("Fail to find out if a tag exists on a card")
+        }
+    }
+
+    /// Set a tag to a given card. Do nothing if it is set already.
+    pub fn set_tag_to_card(&self, tag_name: &str, major_card_number: usize) -> Result<(), &'static str> {
+        let sql = "insert into tag(tag_name, major_card_number) values (?1, ?2);";
+        let args = params![tag_name, major_card_number as u32];
+        let success = self.connection.execute(sql, args);
+        match success {
+            Ok(_) => Ok(()),
+            Err(_) => Err("Fail to set a card to a card"),
+        }
+    }
+
+    /// Unset a tag of the given card.
+    pub fn unset_tag_of_card(&self, tag_name: &str, major_card_number: usize) -> Result<(), &'static str>  {
+        let sql = "delete from tag where tag_name = ?1 and major_card_number = ?2;";
+        let args = params![tag_name, major_card_number as u32];
+        let success = self.connection.execute(sql, args);
+        match success {
+            Ok(_) => Ok(()),
+            Err(_) => Err("Fail to unset a tag"),
+        }
+    }
+
+    pub fn unset_tag_from_all_cards(&self, tag_name: &str) -> Result<(), &'static str> {
+        let sql = "delete from tag where tag_name = ?1";
+        let args = params![tag_name];
+        let success = self.connection.execute(sql, args);
+        println!("tag {} deleted from all cards", tag_name);
+        match success {
+            Ok(_) => Ok(()),
+            Err(_) => Err("Fail to unset tag from all cards"),
+        }
+    }
+
+    /// Latest batch id number or zero if no batches available.
+    pub fn latest_batch_id_or_zero(&self) -> Result<usize, &'static str> {
+        let sql = "select coalesce(max(batch_id), 0) as latest_batch_id from tag_history;";
+        let args = params![];
+        let success = self.connection.query_row(
+            sql,
+            args,
+            |row| {
+                let latest_batch_id: u32 = row.get(0)?;
+                return Ok(latest_batch_id as usize);
+        });
+        match success {
+            Ok(latest_batch_id) => Ok(latest_batch_id),
+            Err(_) => Err("Fail to get the latest batch id"),
+        }
+    }
+
+    /// Insert a tag into a history as a part of a batch.
+    pub fn insert_tag_to_a_batch_history(&self, batch_id: usize, tag_name: &str, major_card_number: usize) -> Result<(), &'static str> {
+        let sql = "insert into tag_history(batch_id, tag_name, major_card_number) values (?1, ?2, ?3);";
+        let args = params![batch_id as u32, tag_name, major_card_number as u32];
+        let success = self.connection.execute(sql, args);
+        match success {
+            Ok(_) => Ok(()),
+            Err(_) => Err("Fail to set a card to a card"),
+        }
+    }
+
+    /// Delete tags from cards in a given batch.
+    pub fn delete_card_tags_in_a_batch(&self, batch_id: usize) -> Result<(), &'static str> {
+        let sql = "delete from tag where (tag_name, major_card_number) in (select tag_name, card from tag_history where batch_id = ?1)";
+        let args = params![batch_id as u32];
+        let success = self.connection.execute(sql, args);
+        match success {
+            Ok(_) => Ok(()),
+            Err(_) => Err("Fail to delete card tags"),
+        }
+    }
+
+    /// Delete the batch from tag history.
+    pub fn delete_batch_from_history(&self, batch_id: usize) -> Result<(), &'static str> {
+        let sql = "delete from tag_history where batch_id = ?1";
+        let args = params![batch_id as u32];
+        let success = self.connection.execute(sql, args);
+        match success {
+            Ok(_) => Ok(()),
+            Err(_) => Err("Fail to delete batch from history"),
+        }
+    }
+}
+
+pub struct Tag {}
+
+impl feature::Feature for Tag {
+    fn enable(&self, conn: &mut Connection) {
+        let success = conn.execute_batch(
+            "
+            create table tag (
+                tag_name text not null,
+                major_card_number integer not null,
+                unique(tag_name, major_card_number)
+            );
+            create table tag_history (
+                batch_id integer not null,
+                tag_name text not null,
+                major_card_number integer not null
+            );
+            "
+        );
+
+        if let Err(msg) = success {
+            panic!("Fail to create tag tables. Reason: {}", msg);
+        }
+    }
+    fn rollback(&self, conn: &mut Connection) {
+        let success = conn.execute_batch(
+            "
+            delete table tag;
+            delete table tag_history;
+            "
+        );
+
+        if let Err(msg) = success {
+            panic!("Fail to delete tag tables. Reason: {}", msg);
         }
     }
 }
---
+++ src/zktag.rs
@@ -0,0 +1,202 @@
+use crate::varg::Args;
+use rusqlite::Connection;
+use std::path::PathBuf;
+use crate::model::tag as tag_lib;
+use crate::card;
+use crate::model;
+
+fn set_tag_to_given_cards(args: &Args) -> Result<(), &'static str> {
+    let mut iter = args.args.iter();
+    let tag_name = iter.next().unwrap();
+    if !tag_lib::is_valid_tag(&tag_name) {
+        eprintln!("Invalid tag name: {}", tag_name);
+        return Err("Invalid tag name")
+    }
+    let cards_iter = iter;
+
+    let timeline_file = args.timeline_file.as_ref().unwrap();
+    let connection = model::open_timeline(timeline_file).unwrap();
+
+    let mut set_tag_commands: Vec<tag_lib::SetTag> = Vec::new();
+
+    for card_name in cards_iter {
+        let card_path = PathBuf::from(card_name);
+        let card_file_name = card_path.file_name().unwrap();
+        let card_file_name: String = card_file_name.to_string_lossy().to_string();
+        let card_face = card::Face::from_name(&card_file_name);
+        if let Some(card_face) = card_face {
+            let maybe_set_tag = tag_lib::SetTag::new(&connection, tag_name, card_face)?;
+            if let Some(set_tag) = maybe_set_tag {
+                set_tag_commands.push(set_tag);
+            }
+        } else {
+            eprintln!("{}", format!("Not a card name: {}", card_file_name));
+        }
+    }
+
+    // Assuming there is no faults, do the side-effect thing.
+
+    for cmd in set_tag_commands {
+        // Todo(wistrandj): NIL argument.
+        cmd.call_once(());
+    }
+
+    Ok(())
+}
+
+fn delete_tag_of_given_cards(connection: &Connection, tag: &str, cards: &[String]) -> Result<(), &'static str> {
+    let mut cmds = Vec::new();
+
+    for card in cards {
+        let card_face = card::Face::from_name(card.as_str());
+        if let Some(card_face) = card_face {
+            let delete_command = tag_lib::DeleteTag::new(connection, tag, card_face)?;
+            cmds.push(delete_command);
+        } else {
+            return Err("Invalid card name given");
+        }
+    }
+
+    for cmd in cmds {
+        cmd.call_once()?;
+    }
+
+    Ok(())
+}
+
+fn delete_whole_tag(connection: &Connection, tag: &str) -> Result<(), &'static str> {
+    println!("01 here");
+    let cmd = tag_lib::DeleteTagAll::new(connection, tag)?;
+    println!("02a here");
+    let r = cmd.call_once();
+    println!("03a here");
+    return r;
+}
+
+fn help_text() {
+    println!("Usage of tag subcommand:");
+    println!("Set the tag DCN1 to cards");
+    println!("  zk -t ./here.zk tag DCN1 15 101");
+    println!("");
+    println!("Show all tags");
+    println!("   zk -t ./here.zk tag --list");
+    println!("");
+    println!("Show tags of the given card");
+    println!("   zk -t ./here.zk tag --list 101");
+    println!("");
+    println!("Delete a tag of the given card");
+    println!("   zk -t ./here.zk tag --delete DCN! 101");
+}
+
+pub fn zktag(timeline: &PathBuf, args: &Args) -> Result<(), &'static str> {
+    // Todo(wistrandj): Feature to rename a tag
+    let parameters = &args.args;
+    if parameters.len() == 0 {
+        println!("Need more arguments");
+    }
+
+    let first_argument = parameters.get(0).unwrap();
+    if first_argument == "--list" || first_argument == "-l" {
+        // Todo(wistrandj): This feature shows all tags. Add a feature to this switch: The user can
+        // give a list of cards. Show only tags on those cards. User does not give any cards: show
+        // all cards.
+        let timeline_file = args.timeline_file.as_ref().unwrap();
+        let connection = model::open_timeline(timeline_file).unwrap();
+
+        if parameters.len() > 1 {  // The first argument is --list or -l
+            // Show tags on the given cards
+            let mut major_numbers = Vec::new();
+
+            for card_name in &parameters[1..] {
+                let cardface = card::Face::from_name(card_name);
+                if let Some(cardface) = cardface {
+                    let major_number = cardface.major_number();
+                    major_numbers.push(major_number);
+                } else {
+                    return Err("Invalid card name given")
+                }
+            }
+
+            let show_tag = tag_lib::ShowTag::new_show_card_tags(&connection, &major_numbers);
+            let tags: Vec<String> = show_tag.call_once()?;
+            for tag in tags {
+                println!("{}", tag);
+            }
+        } else {
+            // Show all tags. User did not give any extra card arguments.
+            let show_tag = tag_lib::ShowTag::new_show_all_tags(&connection);
+            let tags: Vec<String> = show_tag.call_once()?;
+            for tag in tags {
+                println!("{}", tag);
+            }
+        }
+    } else if first_argument == "--delete" || first_argument == "-d" {
+        let timeline_file = args.timeline_file.as_ref().unwrap();
+        let connection = model::open_timeline(timeline_file).unwrap();
+        if let Some(tag_name) = parameters.get(1) {
+            let cards_or_empty_list = &parameters.as_slice()[2..];
+            for c in cards_or_empty_list {
+                println!("........ {}", c);
+            }
+            if cards_or_empty_list.len() == 0 {
+                println!("... Deleting the tag all cards");
+                let success = delete_whole_tag(&connection, tag_name);
+                match success {
+                    Ok(_) => { return Ok(()) },
+                    Err(msg) => {
+                        eprintln!("{}", msg);
+                        return Err(msg);
+                    }
+                }
+            } else {
+                println!("... Deleting tag from few cards");
+                return delete_tag_of_given_cards(&connection, tag_name, cards_or_empty_list);
+            }
+        } else {
+            eprintln!("Missing tag");
+        }
+    } else if first_argument == "--show" || first_argument == "-s" {
+        // Show all tags of given list of cards.
+        let mut user_args_tags = Vec::new();
+        if parameters.len() == 1 {   // The first argument is "--show" or "-s"
+            eprintln!("Missing tag name");
+            return Err("Missing tag name")
+        }
+        for possibly_tag_argument in &parameters[1..] {
+            let tag_argument = String::from(possibly_tag_argument);
+            if tag_lib::is_valid_tag(&tag_argument) {
+                user_args_tags.push(tag_argument);
+            }
+
+            if user_args_tags.len() == 0 {
+                eprintln!("No tags given");
+                return Err("No tags given");
+            }
+
+        }
+        let timeline_file = args.timeline_file.as_ref().unwrap();
+        let connection = model::open_timeline(timeline_file).unwrap();
+        let cmd = tag_lib::ShowAllCardsHavingTag::new(&connection, &user_args_tags);
+
+        let result = cmd.call_once();
+
+        if let Ok(major_card_numbers) = result {
+            for major_card in major_card_numbers {
+                println!("{}", major_card);
+            }
+        } else  if let Err(msg) = result{
+            eprintln!("{}", msg);
+            return Err(msg);
+        }
+    } else if args.args.len() > 1 {
+        // Set a tag to given cards
+        // The first argument has to be the tag name. Rest of them are the cards. Either name of
+        // the cards or a path to each of them.
+        set_tag_to_given_cards(args)?;
+    } else {
+        help_text();
+        return Ok(());
+    }
+
+    Ok(())
+}
